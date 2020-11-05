615beb1b235edf9269cf69d12c38745fa8da344f
brby2549 <55068210+brby2549@users.noreply.github.com>
2020-11-04 19:13:08 -0700

    Add files via upload

---
+++ Water_Airspeed.m
@@ -0,0 +1,28 @@
+%%
+%Water manometer
+%row 1: Pitot : 1-9
+%row 1: Pitot : 1-9
+%row 1: Venturi : 2-10
+%row 1: Pitot : 2-10
+%row 1: Pitot : 2-10
+%row 1: Venturi : 1-9
+%row 1: Pitot : 1-9
+%row 1: Venturi : 2-10
+%row 1: Venturi : 2-10
+%row 1: Venturi : 1.5-9.5
+%row 1: Venturi : 1-9
+%row 1: Pitot : 1-9
+%row 1: Venturi : 2-10
+%row 1: Venturi : 2-10
+%row 1: Pitot : 2-10
+%row 1: Pitot : 1.5-9.5
+%row 1: Pitot : 1.5-9.5
+%row 1: Pitot : 2-10
+%row 1: Pitot : 1.5-9.5
+%row 1: Pitot : 0.5-8.5
+%row 1: Pitot : 0.5-8.5
+%row 1: Venturi : 0.5-8.5
+%row 1: Pitot : 0.5-8.5
+%row 1: Venturi : 0.5-8.5
+%row 1: Venturi : 1.5-9.5
+%row 1: Pitot : 0.5-8.5
