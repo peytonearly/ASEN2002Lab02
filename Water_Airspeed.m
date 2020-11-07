24d364525e6a4d71263dcb4d1674a4b96745a9c1
peytonearly <peyton.early@colorado.edu>
2020-11-06 21:34:07 -0700

    Sorted water manometer data by type of tube used

--- Water_Airspeed.m
+++ Water_Airspeed.m
@@ -1,5 +1,8 @@
-%%
-%Water manometer
+%% Cleaning
+clear all;
+clc;
+
+%% Water manometer
 %row 1: Pitot : 1-9
 %row 1: Pitot : 1-9
 %row 1: Venturi : 2-10
@@ -26,3 +29,15 @@
 %row 1: Venturi : 0.5-8.5
 %row 1: Venturi : 1.5-9.5
 %row 1: Pitot : 0.5-8.5
+
+%% Load Water manometer data
+load('WaterManometerDataFile');
+
+%% Organize table data
+WMR.Properties.VariableNames = {'Timestamp', 'GroupNumber', 'TubeType', 'Voltage1', 'HeightVoltage1', 'Voltage2', 'HeightVoltage2', 'Voltage3', 'HeightVoltage3', 'Voltage4', 'HeightVoltage4', 'Voltage5', 'HeightVoltage5'};
+WMR.TubeType = categorical(WMR.TubeType);
+WMR = sortrows(WMR, {'TubeType', 'Timestamp'});
+
+%% Calculations
+% Pgage = rho * g * h
+% Pabs = Patm + Pgage
