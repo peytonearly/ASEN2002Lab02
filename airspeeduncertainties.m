c7bfd3de8045754586b750fb08d4998d477e302c
peytonearly <peyton.early@colorado.edu>
2020-11-18 11:00:13 -0700

    idk what this is for it doesnt do anything

--- airspeeduncertainties.m
+++ airspeeduncertainties.m
@@ -8,4 +8,3 @@ dvdp = ((R*Tatm)/(Patm*(1-((A2/A1)^2))))*sqrt((Patm*(1-((A2/A1)^2)))/(2*p*R*Tatm
 dvdTatm = ((p*R)/(Patm(1-((A2/A1)^2))))*sqrt((Patm*(1-((A2/A1)^2)))/(2*p*R*Tatm));
 dvdPatm = (sqrt((Patm*(1-((A2/A1)^2)))/(2*p*R*Tatm)))*((p*R*Tatm)/(1-((A2/A1)^2)))*(-(Patm^2));
 dv = sqrt(((dvdp*delp)^2)+((dvdTatm*delT)^2)+((dvdPatm*delPatm)^2));
-
