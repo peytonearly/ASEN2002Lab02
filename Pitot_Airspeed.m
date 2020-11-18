4604a0e1b15a54ae1adba0e488911234525bbbfe
brby2549 <55068210+brby2549@users.noreply.github.com>
2020-11-18 12:17:29 -0700

    Add files via upload

--- Pitot_Airspeed.m
+++ Pitot_Airspeed.m
@@ -70,72 +70,93 @@ A = mean(P1D5(:,1)); % Airspeed Differential Pressure [Pa]
 %B = mean(P1D5(:,2)); % Aux Differential Pressure [Pa]
 %dP = A - B;   %delta P
 S_P1D5 = BernoulliEq(A, R, Tatm,Patm);
+E(1) = error(A,Tatm,Patm);
 
 %VOLTAGE = 1
 A = mean(P1(:,1));
 %B = mean(P1(:,2));
 %dP = A - B;   %delta P
 S_P1 = BernoulliEq(A, R, Tatm,Patm);
+E(2) = error(A,Tatm,Patm);
 
 %VOLTAGE = 3.5
 A = mean(P3D5(:,1));
 %B = mean(P3D5(:,2));
 %dP = A - B;   %delta P
 S_P3D5 = BernoulliEq(A, R, Tatm,Patm);
+E(3) = error(A,Tatm,Patm);
 
 %VOLTAGE = 3
 A = mean(P3(:,1));
 %B = mean(P3(:,2));
 %dP = A - B;   %delta P
 S_P3 = BernoulliEq(A, R, Tatm,Patm);
+E(4) = error(A,Tatm,Patm);
 
 %VOLTAGE = 5.5
 A = mean(P5D5(:,1));
 %B = mean(P5D5(:,2));
 %dP = A - B;   %delta P
 S_P5D5 = BernoulliEq(A, R, Tatm,Patm);
+E(5) = error(A,Tatm,Patm);
 
 %VOLTAGE = 5
 A = mean(P5(:,1));
 %B = mean(P5(:,2));
 %dP = A - B;   %delta P
 S_P5 = BernoulliEq(A, R, Tatm,Patm);
+E(6) = error(A,Tatm,Patm);
 
 %VOLTAGE = 7.5
 A = mean(P7D5(:,1));
 %B = mean(P7D5(:,2));
 %dP = A - B;   %delta P
 S_P7D5 = BernoulliEq(A, R, Tatm,Patm);
+E(7) = error(A,Tatm,Patm);
 
 %VOLTAGE = 7
 A = mean(P7(:,1));
 %B = mean(P7(:,2));
 %dP = A - B;   %delta P
 S_P7 = BernoulliEq(A, R, Tatm,Patm);
+E(8) = error(A,Tatm,Patm);
 
 %VOLTAGE = 9.5
 A = mean(P9D5(:,1));
 %B = mean(P9D5(:,2));
 %dP = A - B;   %delta P
 S_P9D5 = BernoulliEq(A, R, Tatm,Patm);
+E(9) = error(A,Tatm,Patm);
 
 %VOLTAGE = 9
 A = mean(P9(:,1));
 %B = mean(P9(:,2));
 %dP = A - B;   %delta P
 S_P9 = BernoulliEq(A, R, Tatm,Patm);
+E(10) = error(A,Tatm,Patm);
 
 %%
 % Airspeed vs. Voltage
 Voltage = [1 1.5 3 3.5 5 5.5 7 7.5 9 9.5];
 Airspeed = [S_P1 S_P1D5 S_P3 S_P3D5 S_P5 S_P5D5 S_P7 S_P7D5 S_P9 S_P9D5];
 
-plot(Voltage, Airspeed);
+errorbar(Voltage, Airspeed,E);
 xlabel('Voltage [V]');
 ylabel('Airspeed [m/s]');
 title('Pitot Airspeed vs Voltage');
 
 %%
+function dv1 = error(p,Tatm,Patm)
+    R = 8.314;%[J/mol.K]
+    delT = 1/4;%[deg C]
+    delp = 0.01*6894.76; %[pa]
+    delPatm = 3.75;%[1.5% of 250]
+    
+    dvdp1 = ((R*Tatm)/Patm)/sqrt(2*p*((R*Tatm)/Patm));
+    dvdTatm1 = ((p*R)/Patm)/sqrt(2*p*((R*Tatm)/Patm));
+    dvdPatm1 = (-(p*R*Tatm)/(Patm^2))/sqrt(2*p*((R*Tatm)/Patm));
+    dv1 = sqrt(((dvdp1*delp)^2)+((dvdTatm1*delT)^2)+((dvdPatm1*delPatm)^2));
+end
 %Function
 function output = BernoulliEq(deltaP, R, Tatm, Patm)
      output = sqrt(2*deltaP*((R*Tatm)/Patm));
--- Venturi_Airspeed.m
+++ Venturi_Airspeed.m
@@ -23,9 +23,7 @@ I = 500;
 %average value of the atmospheric temperature and pressure 
 Patm = (sum(VV1(:,1)) + sum(VV2(:,1)) + sum(VV3(:,1)) + sum(VV4(:,1)) + sum(VV5(:,1)) + sum(VV6(:,1)) + sum(VV7(:,1)) + sum(VV8(:,1)) + sum(VV9(:,1)) + sum(VV10(:,1)) + sum(VV11(:,1)) + sum(VV12(:,1))) / (N*12);
 Tatm = (sum(VV1(:,2)) + sum(VV2(:,2)) + sum(VV3(:,2)) + sum(VV4(:,2)) + sum(VV5(:,2)) + sum(VV6(:,2)) + sum(VV7(:,2)) + sum(VV8(:,2)) + sum(VV9(:,2)) + sum(VV10(:,2)) + sum(VV11(:,2)) + sum(VV12(:,2))) / (N*12);
-
 R = 8.314; %[J/mol·K]
-
 ratio = 1/9.5;
 %%
 % Obtaining the various pressures with respect to their voltage
@@ -71,72 +69,95 @@ A = mean(VD5(:,1)); % Airspeed Differential Pressure [Pa]
 %B = mean(VD5(:,2)); % Aux Differential Pressure [Pa]
 %dP = A - B;   %delta P
 S_VD5 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(1) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 2
 A = mean(V2(:,1));
 %B = mean(V2(:,2));
 %dP = A - B;   %delta P
 S_V2 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(2) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 2.5
 A = mean(V2D5(:,1));
 %B = mean(V2D5(:,2));
 %dP = A - B;   %delta P
 S_V2D5 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(3) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 4
 A = mean(V4(:,1));
 %B = mean(V4(:,2));
 %dP = A - B;   %delta P
 S_V4 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(4) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 4.5
 A = mean(V4D5(:,1));
 %B = mean(V4D5(:,2));
 %dP = A - B;   %delta P
 S_V4D5 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(5) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 6 
 A = mean(V6(:,1));
 %B = mean(V6(:,2));
 %dP = A - B;   %delta P
 S_V6 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(6) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 6.5
 A = mean(V6D5(:,1));
 %B = mean(V6D5(:,2));
 %dP = A - B;   %delta P
 S_V6D5 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(7) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 8
 A = mean(V8(:,1));
 %B = mean(V8(:,2));
 %dP = A - B;   %delta P
 S_V8 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(8) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 8.5
 A = mean(V8D5(:,1));
 %B = mean(V8D5(:,2));
 %dP = A - B;   %delta P
 S_V8D5 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(9) = error(A,Tatm,Patm,ratio);
 
 %VOLTAGE = 10
 A = mean(V10(:,1));
 %B = mean(V10(:,2));
 %dP = A - B;   %delta P
 S_V10 = VenturiTubeConfig(A, R, Tatm,Patm,ratio);
+E(10) = error(A,Tatm,Patm,ratio);
 
 %%
 % Airspeed vs. Voltage
 Voltage = [0.5 2 2.5 4 4.5 6 6.5 8 8.5 10];
 Airspeed = [S_V2 S_V2 S_V2D5 S_V4 S_V4D5 S_V6 S_V6D5 S_V8 S_V8D5 S_V10];
 
-plot(Voltage, Airspeed);
+errorbar(Voltage, Airspeed,E);
 xlabel('Voltage [V]');
 ylabel('Airspeed [m/s]');
 title('Venturi Airspeed vs Voltage');
 
+
 %%
+function dv = error(p,Tatm,Patm,ratio)
+    R = 8.314;%[J/mol.K]
+    delT = 1/4;%[deg C]
+    delp = 0.01*6894.76; %[pa]
+    delPatm = 3.75;%[1.5% of 250]
+    
+    dvdp = ((R*Tatm)/(Patm*(1-((ratio)^2))))*sqrt((Patm*(1-((ratio)^2)))/(2*p*R*Tatm));
+    dvdTatm = ((p*R)/(Patm*(1-((ratio)^2))))*sqrt((Patm*(1-((ratio)^2)))/(2*p*R*Tatm));
+    dvdPatm = (sqrt((Patm*(1-((ratio)^2)))/(2*p*R*Tatm)))*((p*R*Tatm)/(1-((ratio)^2)))/(-(Patm^2));
+    dv = sqrt(((dvdp*delp)^2)+((dvdTatm*delT)^2)+((dvdPatm*delPatm)^2));
+end
+
 %Function
 function output = VenturiTubeConfig(deltaP, R, Tatm, Patm, ratio) 
     output = sqrt((2 * deltaP * R * Tatm) / (Patm * (1 - (ratio)^2)));
