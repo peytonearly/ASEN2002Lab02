4b12969f75eae823e0d7d2a7d33bc3d1952af499
peytonearly <peyton.early@colorado.edu>
2020-11-25 12:28:35 -0700

    Made progress on part 6

--- Part06_MeasurementofBoundaryLayer.m
+++ Part06_MeasurementofBoundaryLayer.m
@@ -5,4 +5,31 @@ clc;
 %% Load Port Data
 load('PitotProbeDataFile.mat');
 
-%% 
+%% Calculate density and velocity
+S = struct('Tables', {PP1, PP2, PP3, PP4, PP5, PP6, PP7, PP8, PP9, PP10, PP1, PP12}); % Put tables into struct to be easily looped
+R = 287; % Gas constant [J/kgK]
+
+for i = 1:length(S)
+   t = S(i).Tables; % Assign current table into temporary variable
+   t.Density = t.x_AtmosphericPressure_Pa_ ./ (R .* t.AtmosphericTemperature_K_); % Calculate density for each measurement
+   t.FreestreamVelocity = sqrt((2 .* t.AirspeedDifferentialPressure_Pa_) ./ t.Density);
+   t.BoundaryLayerVelocity = sqrt((2 .* t.AuxDifferentialPressure_Pa_) ./ t.Density);
+   t = sortrows(t, {'ELDProbeYAxis_mm_'});
+   S(i).Tables = t;
+end
+
+T = vertcat(S(1).Tables, S(2).Tables, S(3).Tables, S(4).Tables, S(5).Tables, S(6).Tables, S(7).Tables, S(8).Tables, S(9).Tables, S(10).Tables, S(11).Tables, S(12).Tables);
+T = sortrows(T, {'Voltage_V_', 'ELDProbeYAxis_mm_'});
+
+%% Plot Results
+for i = 1:length(S)
+    subplot(3, 4, i);
+    plot(S(i).Tables.ELDProbeYAxis_mm_, S(i).Tables.FreestreamVelocity);
+    hold on;
+    plot(S(i).Tables.ELDProbeYAxis_mm_, S(i).Tables.BoundaryLayerVelocity);
+    hold off;
+    title("Velocty vs Y Location");
+    xlabel("ELD Probe Y-Location [mm]");
+    ylabel("Velocity [m/s]");
+    legend('Freestream', 'Boundary Layer');
+end
