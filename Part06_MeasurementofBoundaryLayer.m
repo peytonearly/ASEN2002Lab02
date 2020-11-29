0e3f9beb0c4d062997b3838cc0fe79807f385234
Peyton Early <64031475+peytonearly@users.noreply.github.com>
2020-11-29 15:31:08 -0700

    Made a little bit of progress on part 6

--- Part06_MeasurementofBoundaryLayer.m
+++ Part06_MeasurementofBoundaryLayer.m
@@ -14,12 +14,13 @@ for i = 1:length(S)
    t.Density = t.x_AtmosphericPressure_Pa_ ./ (R .* t.AtmosphericTemperature_K_); % Calculate density for each measurement
    t.FreestreamVelocity = sqrt((2 .* t.AirspeedDifferentialPressure_Pa_) ./ t.Density);
    t.BoundaryLayerVelocity = sqrt((2 .* t.AuxDifferentialPressure_Pa_) ./ t.Density);
+   t.Port = i * ones(2500, 1);
    t = sortrows(t, {'ELDProbeYAxis_mm_'});
    S(i).Tables = t;
 end
 
 T = vertcat(S(1).Tables, S(2).Tables, S(3).Tables, S(4).Tables, S(5).Tables, S(6).Tables, S(7).Tables, S(8).Tables, S(9).Tables, S(10).Tables, S(11).Tables, S(12).Tables);
-T = sortrows(T, {'Voltage_V_', 'ELDProbeYAxis_mm_'});
+T = sortrows(T, {'Port', 'Voltage_V_', 'ELDProbeYAxis_mm_'});
 
 %% Plot Results
 for i = 1:length(S)
@@ -28,8 +29,18 @@ for i = 1:length(S)
     hold on;
     plot(S(i).Tables.ELDProbeYAxis_mm_, S(i).Tables.BoundaryLayerVelocity);
     hold off;
-    title("Velocty vs Y Location");
+    title("Velocty vs Y Location - Port " + i);
     xlabel("ELD Probe Y-Location [mm]");
     ylabel("Velocity [m/s]");
     legend('Freestream', 'Boundary Layer');
 end
+
+%% Calculate Boundary Layer
+% Find velocity at edge of boundary layer
+T.BoundaryLayerEdgeVelocity = T.FreestreamVelocity .* 0.95; % Boundary layer velocity is 95% of freestream velocity
+
+% Calculate area at end of boundary layer?
+% A1V1 = A2V2 --> A2 = A1V1/V2
+T.ATestSection = 144 * ones(height(T), 1); % Area of test section in in^2
+T.BoundaryLayerArea = (T.ATestSection .* T.FreestreamVelocity) ./ T.BoundaryLayerEdgeVelocity;
+T.HeightBoundaryLayer = T.BoundaryLayerArea .^ .5 - T.ATestSection .^ .5;
